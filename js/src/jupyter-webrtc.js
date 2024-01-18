49946bbbfa42cd6979113f1657d3424251af6427
gjmooney <gjmooney@asu.edu>
2024-01-18 14:22:28 +0100

    Split out Media

--- js/src/jupyter-webrtc.js
+++ js/src/jupyter-webrtc.js
@@ -1,6 +1,7 @@
 export * from "./streams/Audio";
 export * from "./streams/Camera";
 export * from "./streams/Image";
+export * from "./streams/Media";
 export * from "./streams/Recorder";
 export * from "./streams/Video";
 export * from "./streams/Webrtc";
